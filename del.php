140b99cde8b29b6b87583e1dd48abdd0ff67bd98
Hellbao <1573962958@qq.com>
2018-12-13 10:45:04 +0800

    华为主题个人开发环境 提交次数 1

---
+++ del.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zzz
+ * Date: 2018-12-04
+ * Time: 15:27
+ */
+function echoMsgz($msg) {
+    echo iconv("UTF-8", "gb2312", "\r\r\n" . $msg );
+}
+
+function echoz() {
+    echo "\r\r\n----------------------------------------------------------------------\r\r\n";
+}
+echoz();
+//开始
+$dir = dirname(__FILE__);
+$file = scandir($dir);
+foreach ($file as $v) {
+    $extend = pathinfo($v);
+    if ($extend['extension'] == "hwt") {
+        echo echoMsgz("【1】：读取文件：".$v."   信息：");
+        system('adb shell rm /sdcard/Huawei/Themes/'.$v);
+        echo echoMsgz("【2】：删除文件 ".$v.'  ==> /sdcard/Huawei/Themes/');
+    }
+}
+
+//system('adb shell input keyevent 26');
+//system('adb shell input swipe 540 1000 540 500');
+echo echoMsgz("【3】：点击HOME，返回桌面");
+system('adb shell input keyevent 3');
+echo echoMsgz("【4】：杀死运行着的 [主题] 进程");
+system('adb shell am force-stop com.huawei.android.thememanager');
+echo echoMsgz("【5】：点击 [主题] 图标");
+system('adb shell input tap 950 2066');
+sleep(3);
+echo echoMsgz("【6】：点击右下角 [我的] 图标");
+system('adb shell input tap 980 2150');
+echo echoMsgz("【7】：点击 [我的主题] 选项");
+system('adb shell input tap 320 710');
+system('adb shell input tap 200 1200');
+system('adb shell input tap 960 2188');
+system('adb shell input tap 840 2090');
+//结束
+echoz();
